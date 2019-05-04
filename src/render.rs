10039e27d8c843efd64da6ec0aa537991427cc30
Misha Wagner <mishajw@gmail.com>
2019-05-04 18:59:46 +0100

    Add implementation documentation to rendering

--- src/render.rs
+++ src/render.rs
@@ -28,11 +28,11 @@ pub fn render(
     // For every pixel...
     for image_x in 0..width {
         for image_y in 0..height {
-            // Calculate the vector from the camera centre to the correct point
-            // on the image plane
             // Need to offset to make the centre of the image zero
             let offset_x_pixels = image_x as f64 - (width / 2) as f64;
             let offset_y_pixels = image_y as f64 - (height / 2) as f64;
+            // Calculate the ray from the camera centre to the correct point
+            // on the image plane
             let image_point = image_plane_centre
                 + (image_plane_x_unit * offset_x_pixels as f64 * pixel_size)
                 + (image_plane_y_unit * offset_y_pixels as f64 * pixel_size);
@@ -40,13 +40,16 @@ pub fn render(
                 world.camera.position,
                 (image_point - world.camera.position).unit(),
             );
+            // Get what the ray collides with
             let trace_result = tracer.trace(&ray);
             let color = match trace_result {
+                // If it does collide, ask the collided shape what color to draw
                 Some(result) => result.shape.get_color(
                     &ray,
                     result.collision_position,
                     &tracer,
                 ),
+                // If it doesn't collide, use the background color
                 None => world.background_color,
             };
             image.put_pixel(
@@ -56,6 +59,7 @@ pub fn render(
             )
         }
     }
+    // Save the rendered image
     // TODO: Replace with Result<>
     image.save(output_path).expect("Failed to save image");
 }
